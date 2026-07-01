6cf110eedfb23f8ee8c7ed6971671fd691f6a70b
Gabriel Wederson <gabrielwederson582@gmail.com>
2026-07-01 13:29:04 -0300

    Created: findByPriority, findByTye,FindAllOrderByPriority,markAsInProgress methods

--- help-desk-pro/src/main/java/io/github/gabrielwederson/help_desk_pro/repository/TicketRepository.java
+++ help-desk-pro/src/main/java/io/github/gabrielwederson/help_desk_pro/repository/TicketRepository.java
@@ -1,9 +1,41 @@
 package io.github.gabrielwederson.help_desk_pro.repository;
 
 import io.github.gabrielwederson.help_desk_pro.model.Ticket;
+import io.github.gabrielwederson.help_desk_pro.model.enums.Priority;
+import io.github.gabrielwederson.help_desk_pro.model.enums.Type;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket,Long> {
+
+    @Query("SELECT t FROM Ticket t WHERE t.priority = :priority")
+    Optional<Ticket> findByPriority(@Param("priority")Priority priority);
+
+    @Query("SELECT t FROM Ticket t WHERE t.type = :type")
+    Optional<Ticket> findByType(@Param("type") Type type);
+
+    @Query("""
+    SELECT t
+    FROM Ticket t
+    ORDER BY
+        CASE t.priority
+            WHEN io.github.gabrielwederson.help_desk_pro.model.enums.Priority.HIGH THEN 1
+            WHEN io.github.gabrielwederson.help_desk_pro.model.enums.Priority.MEDIUM THEN 2
+            WHEN io.github.gabrielwederson.help_desk_pro.model.enums.Priority.LOW THEN 3
+        END
+    """)
+    List<Ticket> findAllOrderByPriority();
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Ticket t SET t.status = io.github.gabrielwederson.help_desk_pro.model.enums.Status.IN_PROGRESS WHERE t.id =:id")
+    void markAsInProgress(@Param("id") Long id);
 }
