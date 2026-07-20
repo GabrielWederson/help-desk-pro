c38644f6b2758603e4a88705374d5ea025f0bc1a
Gabriel Wederson <gabrielwederson582@gmail.com>
2026-07-20 11:14:20 -0300

    Created: Consumer RabbitMQ conig

--- notification/src/main/java/io/github/gabrielwederson/notification/config/RabbitMQConfiguration.java
+++ notification/src/main/java/io/github/gabrielwederson/notification/config/RabbitMQConfiguration.java
@@ -1,4 +1,41 @@
 package io.github.gabrielwederson.notification.config;
 
-public class RabbitMQConiguration {
+import org.springframework.amqp.core.AcknowledgeMode;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfiguration {
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate();
+
+        rabbitTemplate.setConnectionFactory(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory) {
+
+        SimpleRabbitListenerContainerFactory factory =
+                new SimpleRabbitListenerContainerFactory();
+
+        factory.setConnectionFactory(connectionFactory);
+        factory.setAcknowledgeMode(AcknowledgeMode.MANUAL);
+
+        return factory;
+    }
 }
---
+++ notification/src/test/java/io/github/gabrielwederson/notification/service/NotificationServiceTest.java
@@ -0,0 +1,4 @@
+package io.github.gabrielwederson.notification.service;
+
+public class NotificationServiceTest {
+}
