8c36cf586c8567e47441c32a51ab02c3a89d6c39
Gabriel Wederson <gabrielwederson582@gmail.com>
2026-07-20 10:55:33 -0300

    Created: response of register method

--- help-desk-pro/src/main/java/io/github/gabrielwederson/help_desk_pro/dto/security/RegisterResponseDTO.java
+++ help-desk-pro/src/main/java/io/github/gabrielwederson/help_desk_pro/dto/security/RegisterResponseDTO.java
@@ -1,4 +1,26 @@
 package io.github.gabrielwederson.help_desk_pro.dto.security;
 
 public class RegisterResponseDTO {
+
+    private Long id;
+    private String email;
+    private String name;
+
+    public RegisterResponseDTO(Long id, String email, String name) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
