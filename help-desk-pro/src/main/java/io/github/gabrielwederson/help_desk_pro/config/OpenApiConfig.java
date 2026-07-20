acbb69dffa93359813a21b28671c86d6cb857ef1
Gabriel Wederson <gabrielwederson582@gmail.com>
2026-07-20 10:44:24 -0300

    Created: open api configuration

--- help-desk-pro/src/main/java/io/github/gabrielwederson/help_desk_pro/config/OpenApiConfig.java
+++ help-desk-pro/src/main/java/io/github/gabrielwederson/help_desk_pro/config/OpenApiConfig.java
@@ -1,4 +1,25 @@
 package io.github.gabrielwederson.help_desk_pro.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenApiConfig {
+
+    @Bean
+    OpenAPI customOpenApi(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Help Desk Pro")
+                        .version("v1")
+                        .description("Help Desk REST API for ticket management")
+                        .termsOfService("https://github.com/GabrielWederson")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://github.com/GabrielWederson")));
+    }
+
 }
