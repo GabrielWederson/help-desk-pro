7a352927ffee5fb649f6a60681225ebd171f2c8d
Gabriel Wederson <gabrielwederson582@gmail.com>
2026-07-20 11:20:55 -0300

    Created: tests for all service scenarios

--- notification/src/test/java/io/github/gabrielwederson/notification/service/EmailServiceTest.java
+++ notification/src/test/java/io/github/gabrielwederson/notification/service/EmailServiceTest.java
@@ -1,4 +1,67 @@
 package io.github.gabrielwederson.notification.service;
 
-public class EmailServiceTest {
+import io.github.gabrielwederson.notification.service.email.EmailService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class EmailServiceTest {
+
+    @Mock
+    private JavaMailSender mailSender;
+
+    @InjectMocks
+    private EmailService emailService;
+
+    @Test
+    void shouldSendEmailSuccessfully() {
+
+        emailService.sendEmailSync(
+                "gabriel@test.com",
+                "Subject Test",
+                "Body Test"
+        );
+
+        ArgumentCaptor<SimpleMailMessage> captor =
+                ArgumentCaptor.forClass(SimpleMailMessage.class);
+
+        verify(mailSender).send(captor.capture());
+
+        SimpleMailMessage message = captor.getValue();
+
+        assertEquals("gabriel@test.com", message.getTo()[0]);
+        assertEquals("Subject Test", message.getSubject());
+        assertEquals("Body Test", message.getText());
+    }
+
+    @Test
+    void shouldCallSendEmailSyncWhenUsingAsyncMethod() {
+
+        EmailService spy = spy(emailService);
+
+        doNothing().when(spy)
+                .sendEmailSync(anyString(), anyString(), anyString());
+
+        spy.sendEmail(
+                "gabriel@test.com",
+                "Subject",
+                "Body"
+        );
+
+        verify(spy).sendEmailSync(
+                "gabriel@test.com",
+                "Subject",
+                "Body"
+        );
+    }
 }
--- notification/src/test/java/io/github/gabrielwederson/notification/service/NotificationServiceTest.java
+++ notification/src/test/java/io/github/gabrielwederson/notification/service/NotificationServiceTest.java
@@ -1,4 +1,81 @@
 package io.github.gabrielwederson.notification.service;
 
-public class NotificationServiceTest {
+import com.rabbitmq.client.Channel;
+import io.github.gabrielwederson.notification.service.email.EmailService;
+import io.github.gabrielwederson.notification.service.listener.NotificationService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class NotificationServiceTest {
+
+    @Mock
+    private EmailService emailService;
+
+    @Mock
+    private Channel channel;
+
+    @InjectMocks
+    private NotificationService notificationService;
+
+    @Test
+    void shouldAckMessageWhenEmailIsSent() throws Exception {
+
+        MessageProperties properties = new MessageProperties();
+        properties.setDeliveryTag(1L);
+
+        Message message = new Message(new byte[0], properties);
+
+        notificationService.ticketComplete(
+                "gabriel@test.com",
+                message,
+                channel
+        );
+
+        verify(emailService).sendEmail(
+                eq("gabriel@test.com"),
+                anyString(),
+                anyString()
+        );
+
+        verify(channel).basicAck(1L, false);
+
+        verify(channel, never())
+                .basicNack(anyLong(), anyBoolean(), anyBoolean());
+    }
+
+    @Test
+    void shouldNackMessageWhenEmailThrowsException() throws Exception {
+
+        MessageProperties properties = new MessageProperties();
+        properties.setDeliveryTag(1L);
+
+        Message message = new Message(new byte[0], properties);
+
+        doThrow(new RuntimeException())
+                .when(emailService)
+                .sendEmail(anyString(), anyString(), anyString());
+
+        notificationService.ticketComplete(
+                "gabriel@test.com",
+                message,
+                channel
+        );
+
+        verify(channel).basicNack(
+                1L,
+                false,
+                false
+        );
+
+        verify(channel, never())
+                .basicAck(anyLong(), anyBoolean());
+    }
 }
