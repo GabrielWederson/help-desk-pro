360c37e00d62b2f69245b4b179531236b2f73675
Gabriel Wederson <gabrielwederson582@gmail.com>
2026-07-20 10:50:27 -0300

    Created: dto for requests in MarkAnIsComplet method

--- help-desk-pro/src/main/java/io/github/gabrielwederson/help_desk_pro/dto/MarkTicketDTO.java
+++ help-desk-pro/src/main/java/io/github/gabrielwederson/help_desk_pro/dto/MarkTicketDTO.java
@@ -1,4 +1,44 @@
 package io.github.gabrielwederson.help_desk_pro.dto;
 
-public class MarkTicketDTO {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class MarkTicketDTO implements Serializable {
+
+    private static final Long serialVersionID = 1L;
+
+    private Long id;
+
+    private String email;
+
+    public MarkTicketDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        MarkTicketDTO that = (MarkTicketDTO) o;
+        return Objects.equals(id, that.id) && Objects.equals(email, that.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email);
+    }
 }
