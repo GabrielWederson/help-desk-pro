c875e591623c04e03a8bff2ac6aadfa99eb60dc5
Gabriel Wederson <gabrielwederson582@gmail.com>
2026-07-20 10:55:50 -0300

    Created: request of register method

--- help-desk-pro/src/main/java/io/github/gabrielwederson/help_desk_pro/dto/security/RegisterRequest.java
+++ help-desk-pro/src/main/java/io/github/gabrielwederson/help_desk_pro/dto/security/RegisterRequest.java
@@ -1,4 +1,6 @@
 package io.github.gabrielwederson.help_desk_pro.dto.security;
 
-public record RegisterRequest() {
+import jakarta.validation.constraints.*;
+
+public record RegisterRequest(@NotBlank @Email String email, @NotBlank String name, @NotBlank String password) {
 }
