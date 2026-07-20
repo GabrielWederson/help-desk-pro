ccda5059e22435b6ab6ee008e88ce55c01eecbfb
Gabriel Wederson <gabrielwederson582@gmail.com>
2026-07-20 10:45:54 -0300

    Created: SecurityConfig configured

--- help-desk-pro/src/main/java/io/github/gabrielwederson/help_desk_pro/config/SecurityConfig.java
+++ help-desk-pro/src/main/java/io/github/gabrielwederson/help_desk_pro/config/SecurityConfig.java
@@ -1,4 +1,104 @@
 package io.github.gabrielwederson.help_desk_pro.config;
 
+import io.github.gabrielwederson.help_desk_pro.security.jwt.JwtTokenFilter;
+import io.github.gabrielwederson.help_desk_pro.security.jwt.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
+@Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    public SecurityConfig(JwtTokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        PasswordEncoder pbkdf2Encoder = new Pbkdf2PasswordEncoder(
+                "", 8, 185000,
+                Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        encoders.put("pbkdf2", pbkdf2Encoder);
+        DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
+
+        passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);
+        return passwordEncoder;
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        JwtTokenFilter filter = new JwtTokenFilter(tokenProvider);
+
+        return http
+                .cors(withDefaults())
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                "/auth/register",
+                                "/auth/signin",
+                                "/auth/refresh",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**"
+                        ).permitAll()
+
+                        .requestMatchers(HttpMethod.POST, "api/ticket/v1")
+                        .hasRole("USER")
+
+                        .requestMatchers(HttpMethod.GET, "api/ticket/v1")
+                        .hasRole("ADMIN")
+
+                        .requestMatchers(HttpMethod.GET, "api/ticket/v1/**")
+                        .hasRole("ADMIN")
+
+                        .requestMatchers(HttpMethod.DELETE, "api/ticket/v1")
+                        .hasAnyRole("USER", "ADMIN")
+
+                        .requestMatchers(HttpMethod.PATCH, "api/ticket/v1/**")
+                        .hasRole("ADMIN")
+
+                        .requestMatchers(HttpMethod.PATCH, "api/ticket/v1")
+                        .hasRole("ADMIN")
+
+                        .requestMatchers(HttpMethod.PUT, "api/ticket/v1")
+                        .hasRole("USER")
+
+                        .requestMatchers("/api/**").authenticated()
+
+                        .requestMatchers("/users").denyAll()
+
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
 }
