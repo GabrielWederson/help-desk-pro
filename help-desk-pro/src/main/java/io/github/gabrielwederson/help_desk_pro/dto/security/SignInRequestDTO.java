35b634ba14fdd2f26edd1424ede7415a5964c899
Gabriel Wederson <gabrielwederson582@gmail.com>
2026-07-20 10:54:36 -0300

    Created: dto for signin method

--- help-desk-pro/src/main/java/io/github/gabrielwederson/help_desk_pro/dto/security/SignInRequestDTO.java
+++ help-desk-pro/src/main/java/io/github/gabrielwederson/help_desk_pro/dto/security/SignInRequestDTO.java
@@ -1,4 +1,7 @@
 package io.github.gabrielwederson.help_desk_pro.dto.security;
 
-public record SignInRequestDTO() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+public record SignInRequestDTO(@NotNull @Email String email, @NotNull String password) {
 }
